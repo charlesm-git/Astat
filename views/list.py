b0a62bd91e2cdf008cf4d905afa2698d1166ad73
cmareau-git <charles_mareau@hotmail.fr>
2025-01-23 12:23:14 +0100

    Implementation of recycle view for the list of boulders. Adaptive height for each line still not working

--- views/list.py
+++ views/list.py
@@ -1,7 +1,7 @@
 from sqlalchemy import select, desc
 
 from kivymd.app import MDApp
-from kivy.properties import StringProperty
+from kivy.properties import StringProperty, BooleanProperty
 from kivymd.uix.screen import MDScreen
 from kivymd.uix.boxlayout import MDBoxLayout
 from kivymd.uix.label import MDLabel
@@ -16,13 +16,13 @@ class AscentItem(MDBoxLayout):
     grade = StringProperty()
     area = StringProperty()
     date = StringProperty()
+    is_group = BooleanProperty()
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
 
 class AscentManager(MDScreen):
-    sort_by = "date"
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
@@ -33,33 +33,34 @@ class AscentManager(MDScreen):
             # Query the ascents with the right ordering requirement
             ascents = session.scalars(ordered_query).all()
 
-            # Clear the scrolling list
-            self.ids.ascent_list.clear_widgets()
+            previous_group = None
+            ascent_data = []
 
-            for i, ascent in enumerate(ascents):
-                # Get the group of the current ascent
-                group_i = group_label_getter(ascent)
-                # Display a label with the right groupe name if it's the first
-                # ascent logged or if the group changed (ie change of year, of
-                # grade, etc.)
-                if i == 0 or group_label_getter(ascents[i - 1]) != group_i:
-                    group_label = MDLabel(
-                        text=str(group_i),
-                        adaptive_height=True,
-                        bold=True,
-                        font_size="20sp",
-                        padding=[0, 10, 0, 0],
+            for ascent in ascents:
+                current_group = group_label_getter(ascent)
+                if previous_group != current_group:
+                    ascent_data.append(
+                        {
+                            "name": str(current_group),
+                            "grade": "",
+                            "area": "",
+                            "date": "",
+                            "is_group": True,
+                        }
                     )
-                    self.ids.ascent_list.add_widget(group_label)
-                    
-                # Add current ascent to the list
-                ascent_item = AscentItem(
-                    name=ascent.name,
-                    grade=ascent.grade.grade_value,
-                    area=ascent.area.name,
-                    date=str(ascent.ascent_date),
+                    previous_group = current_group
+
+                ascent_data.append(
+                    {
+                        "name": ascent.name,
+                        "grade": ascent.grade.grade_value,
+                        "area": ascent.area.name,
+                        "date": str(ascent.ascent_date),
+                        "is_group": False,
+                    }
                 )
-                self.ids.ascent_list.add_widget(ascent_item)
+
+                self.ids.ascent_list.data = ascent_data
 
     def load_by_date(self):
         if self.ids.sort_by_date.style == "filled":
